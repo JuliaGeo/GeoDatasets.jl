34e582df2ab6d23a188994ea036a05d37ced1849
Alexander Barth <barth.alexander@gmail.com>
2019-08-30 22:13:06 +0200

    update documentation

--- docs/src/index.md
+++ docs/src/index.md
@@ -2,6 +2,35 @@
 
 GeoDatasets Julia package repo.
 
-```@autodocs
-Modules = [GeoDatasets]
+
+## Land-sea-lake mask
+
+```@docs
+GeoDatasets.landseamask
 ```
+
+Example plot of the coarse resolution land-sea-lake mask (rasterized at 5 arc minutes).
+
+```@example
+using PyPlot, GeoDatasets
+lon,lat,data = GeoDatasets.landseamask(;resolution='c',grid=5)
+pcolormesh(lon,lat,data')
+colorbar(orientation="horizontal")
+gca().set_aspect(1)
+savefig("landseamask-plot.png"); nothing # hide
+```
+
+![](landseamask-plot.png)
+
+Coast-line from this raster data:
+
+```@example
+using PyPlot, GeoDatasets
+clf() # hide
+lon,lat,data = GeoDatasets.landseamask(;resolution='c',grid=5)
+contour(lon,lat,data',[0.5],colors=[[0.2,0.2,0.2]],linewidths=0.5)
+gca().set_aspect(1)
+savefig("landseamask-coastline-plot.png"); nothing # hide
+```
+
+![](landseamask-coastline-plot.png)
--- src/GeoDatasets.jl
+++ src/GeoDatasets.jl
@@ -8,20 +8,18 @@ using CodecZlib
     lon,lat,data = GeoDatasets.landseamask(;resolution='l',grid=5)
 
 Load the land-sea-lake raster from basemap: 0 is ocean, 1 is land and 2 is lake.
-`grid` is the resolution in arc seconds and should be either 1.25, 2.5, 5 or 10.
-The parameter `resolution` should be either 'c','l','i','h' or 'f'.
+`grid` is the resolution in arc minutes and should be either 1.25, 2.5, 5 or 10.
+The parameter `resolution` should be either 'c','l','i','h' or 'f' 
+(standing for crude, low, intermediate, high and full resolution)
 
-The data is downloaded from:
-https://github.com/matplotlib/basemap/tree/master/lib/mpl_toolkits/basemap/data
+The data is downloaded from [basemap](https://github.com/matplotlib/basemap/tree/master/lib/mpl_toolkits/basemap/data).
 
 The data is originally from [GMT](http://gmt.soest.hawaii.edu/) and distributed under the terms of the
 [GPL licences](https://github.com/matplotlib/basemap/blob/a551c2361314670dc7d95143190d3d48ba750d20/LICENSE_data).
 
 ```julia
-using PyPlot
+using GeoDatasets
 lon,lat,data = GeoDatasets.landseamask(;resolution='c',grid=5)
-pcolormesh(lon,lat,data'); colorbar()
-savefig("f-plot.svg"); nothing # hide
 ```
 
 """
