fdb6589f6133742c044b7f761cfc128b77290cbb
Alexander Barth <barth.alexander@gmail.com>
2019-09-02 11:05:14 +0200

    improve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,15 @@ using Test, GeoDatasets
     @test_throws ErrorException GeoDatasets.landseamask(;resolution='c',grid=-999)
     @test_throws ErrorException GeoDatasets.landseamask(;resolution='g',grid=5)
 end
+
+
+@testset "gshhg" begin
+    res = 'c'
+    @test length(GeoDatasets.gshhg(res,[1,5])) > 300
+    @test length(GeoDatasets.WDBII_river(res,[1,2])) > 1000
+    @test length(GeoDatasets.WDBII_border(res,[1])) >  100
+
+    # resolution 'x' does not exists
+    @test_throws ErrorException GeoDatasets.gshhg('x',[1,5])
+end
+
